953edc5ab306886ac0e6dcd48ff5fa19bc876084
agray998 <a.gray.4797@gmail.com>
2022-01-25 14:49:23 +0000

    added type hinting/docstring

--- gradecalc.py
+++ gradecalc.py
@@ -1,4 +1,7 @@
-def gradecalc(name, homework, assessment, exam):
+def gradecalc(name: str, homework: int, assessment: int, exam: int) -> str:
+    '''Calculates a percentage based on three marks, one out of 25,
+    one out of 50 and one out of 100, and determines a grade (A* - D)
+    based on this percentage.'''
     percent = int((homework + assessment + exam) * 100 / 175)
     grades = {100:'A*', 90:'A*', 80:'A', 70:'B', 60:'C', 50:'D'}
     gradebound = percent - (percent % 10)
